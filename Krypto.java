fa249a6fbd569fc14e06459f1dfc33dc8b7062d0
gelonn <gelonn99@gmail.com>
2016-12-07 09:30:35 +0100

    Add files via upload

---
+++ Krypto.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Krypto {
+
+	static int sizeX, sizeY;
+	static char key[] = new char[26];
+	
+	public static void main(String[] args) {
+		for(int i=0; i<26; i++){
+			key[i]= ' ';
+		}
+		
+		System.out.println("What size do you want your crossword to be?");
+		
+		System.out.print("Rows: ");
+		Scanner in = new Scanner(System.in);	
+		sizeY = in.nextInt();
+		System.out.print("Columns: ");
+		sizeX = in.nextInt();
+		
+		int[][] crossword = new int[sizeX][sizeY];
+		
+		System.out.println("Enter the numbers you want to write into your crossword");
+		for(int y=0; y < sizeY; y++){
+			for(int x=0; x < sizeX; x++){
+				System.out.print("Enter number for row "+y+", "+"position"+x+":");
+				crossword[x][y] = in.nextInt();
+			}
+		}
+		while(true){
+			for(int i=0; i<26; i++){
+				if(key[i] != ' '){
+					System.out.println(Integer.toString(i) + " = "+key[i]);
+				}
+			}
+			
+			System.out.println("Your crossword looks like this:");
+			for(int y=0; y < sizeY; y++){
+				for(int x=0; x < sizeX; x++){
+					if(key[crossword[x][y]] != ' '){
+						System.out.print(key[crossword[x][y]]+" ");
+					} 
+					else{
+						System.out.print(crossword[x][y]+ " ");
+					}
+		
+				}
+				System.out.println();
+			}
+			
+			System.out.println("Enter a number and the letter you would like to exchange it for:");
+			int exchange = in.nextInt();
+			String exchangeFor = in.nextLine();
+			key[exchange] = exchangeFor.charAt(1);
+
+		}
+	}
+}
