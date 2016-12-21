a1087ebd5bdaa80fffc0e201cd1535d060501d80
gelonn <gelonn99@gmail.com>
2016-12-21 10:34:15 +0100

    Add files via upload

--- Krypto.java
+++ Krypto.java
@@ -1,40 +1,43 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Krypto {
 
 	static int sizeX, sizeY;
-	static char key[] = new char[26];
+	static char key[] = new char[28];
 	
 	public static void main(String[] args) {
-		for(int i=0; i<26; i++){
+		for(int i=0; i<28; i++){
 			key[i]= ' ';
 		}
 		
+		Scanner in = new Scanner(System.in);
+		
 		System.out.println("What size do you want your crossword to be?");
 		
-		System.out.print("Rows: ");
-		Scanner in = new Scanner(System.in);	
+		System.out.print("Number of rows: ");	
 		sizeY = in.nextInt();
-		System.out.print("Columns: ");
-		sizeX = in.nextInt();
 		
+		System.out.print("Number of columns: ");
+		sizeX = in.nextInt();
+			
 		int[][] crossword = new int[sizeX][sizeY];
 		
 		System.out.println("Enter the numbers you want to write into your crossword");
 		for(int y=0; y < sizeY; y++){
 			for(int x=0; x < sizeX; x++){
-				System.out.print("Enter number for row "+y+", "+"position"+x+":");
+				System.out.print("Enter number for row "+y+", "+"position "+x+": ");
 				crossword[x][y] = in.nextInt();
 			}
 		}
 		while(true){
-			for(int i=0; i<26; i++){
+			for(int i=0; i<28; i++){
 				if(key[i] != ' '){
 					System.out.println(Integer.toString(i) + " = "+key[i]);
 				}
 			}
 			
-			System.out.println("Your crossword looks like this:");
+			System.out.println("Your crossword looks like this: ");
 			for(int y=0; y < sizeY; y++){
 				for(int x=0; x < sizeX; x++){
 					if(key[crossword[x][y]] != ' '){
@@ -48,11 +51,14 @@ public class Krypto {
 				System.out.println();
 			}
 			
-			System.out.println("Enter a number and the letter you would like to exchange it for:");
+			System.out.println("Enter a number and the letter you would like to exchange it for: ");
 			int exchange = in.nextInt();
 			String exchangeFor = in.nextLine();
-			key[exchange] = exchangeFor.charAt(1);
-
+			if(exchange < 28){
+				key[exchange] = exchangeFor.charAt(1);
+			} else{
+				System.out.println("That number is too big!");
+			} 
 		}
 	}
 }
